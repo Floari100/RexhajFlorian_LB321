0b09ccbdf827b0ca0585b04836ff57ae6065322b
Florian Rexhaj <F.Rexhaj.inf23@stud.bbbaden.ch>
2026-01-03 13:11:04 +0100

    feat: ArticleService behind ApiGateway with health + swagger

---
+++ src/ZuckererWarehouse/ArticleService/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Health checks
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Health endpoint
+app.MapHealthChecks("/health");
+
+app.MapGet("/", () => Results.Ok("ArticleService running"));
+
+app.Run();
